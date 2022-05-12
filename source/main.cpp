a8b3b4367d6c9e915e57740cf3e73deded12625b
tottallynotapour <leonard.lesage1@gmail.com>
2022-05-12 14:46:07 -0400

    last commit

--- source/main.cpp
+++ source/main.cpp
@@ -1,3 +1,8 @@
+//Robert, Brian, Hugo, Leonard
+//Projet - robot PID
+//Fonctions demontrees : balance, chute, tourne sur place
+
+
 #include "mbed.h"
 #include "stdio.h"
 #include <cstdint>
@@ -13,7 +18,6 @@
 #define TARGET_RX_PIN USBRX
 #define CMPS_ADDR 0xC0 
 
-
 // Create a BufferedSerial object to be used by the system I/O retarget code.
 static BufferedSerial serial_port(TARGET_TX_PIN, TARGET_RX_PIN, 115200);
 
@@ -22,7 +26,6 @@ I2C cmps12(p9, p10);        // sda, scl
 int process_variable;
 
 char data[31]; 
-// main() runs in its own thread in the OS
 
 //Pin du moteur 
     // Pin controle avant/arriere
@@ -45,13 +48,10 @@ DigitalOut STEP2(p23);
 Timeout t;
 Timer compteur;
 
-float temps, temps2, dtemps, commande,P, I, PI, tmps, frequence, sumx, x, bearing, bearing360, bearing180;
+float temps, temps2, dtemps, commande,P, I, D, PID, tmps, frequence, sumx, x, bearing, bearing360, bearing180, delta_erreur, err, old_erreur;
 int erreur;
 
-
 using namespace std::chrono;
-
-
 
 // Controle du moteur par interruption
 void flip1() {
@@ -72,13 +72,19 @@ int main()
     frequence = (200 * 16);
     tmps = (1/frequence)/2;
     t.attach(&flip1, tmps);
-    compteur.start();
+    compteur.start(); // Commencement du compteur 
+
     while (true) {
         
         cmps12.write(CMPS_ADDR, 0, 1);
         cmps12.read(CMPS_ADDR, data, sizeof(data));
+
+        // Roll
         process_variable = data[5];
+
+        // Bousole
         bearing =  data[1];
+
 
         bearing360 = bearing * 360 / 256;
 
@@ -89,29 +95,29 @@ int main()
             process_variable = process_variable - 256;
 
         erreur = 0 - process_variable;
-        printf("Erreur: %d \r\n", erreur);
 
-
-
+        // Proportionnel
         commande = 22 * erreur;
         P = commande;
+
+        // Delta temp
         compteur.stop();
         dtemps = duration_cast<milliseconds>(compteur.elapsed_time()).count(); //5
         compteur.reset();
         compteur.start();
         dtemps = dtemps/1000;
 
+        // Integral
         x = erreur * dtemps;
         sumx = sumx + x;
-
         I = sumx * 100;
-        PI = P + I;
-        
-        printf("The time taken was %f seconds\n", dtemps);
-        printf("PI : %f \r\n", PI);
-        printf("P : %0.2f", x);
-        printf("I : %0.2f, I", I);
-        printf("bearing : %f \r\n", bearing180);
+
+        // Derive
+        delta_erreur = err - old_erreur;
+        old_erreur = err;
+        D = (delta_erreur / dtemps) * 20;
+
+        PID = P + I + D;
 
         compteur.stop();   //9
         
@@ -120,9 +126,7 @@ int main()
             DIR1 = 0;
             DIR2 = 1;
             EN = 0;
-            frequence = (200 * 16) * (PI / 360) * -1;
-            //tmps = (1/frequence)/2;
-            //t.attach(&flip1, tmps);
+            frequence = (200 * 16) * (PID / 360) * -1;
         }
 
         if (erreur > 5)
@@ -130,18 +134,16 @@ int main()
             DIR1 = 1;
             DIR2 = 0;
             EN = 0;
-            frequence = (200 * 16) * (PI / 360);
-            //tmps = (1/frequence)/2;
-            //t.attach(&flip1, tmps);
+            frequence = (200 * 16) * (PID / 360);
         }
-        /*
-        if (erreur > -5 && erreur < 5 )
+        
+        if (erreur >= -5 && erreur <= 5 )
         {
             EN = 0;
             DIR1 = 1;
             DIR2 = 1;
             frequence = (200 * 16) * (280.0/ 360);
-        }*/
+        }
 
         if (process_variable > 70 || process_variable < -70) //Arret en chute
         {
